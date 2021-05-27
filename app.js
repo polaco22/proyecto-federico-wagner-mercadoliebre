68029e32d35003ea95c310350a0fcffc8c3c8f6e
Federico Wagner <federico.wagner@hotmail.com>
2021-05-27 20:34:06 -0300

    asd

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const path = require('path');
+const fs = require('fs');
 const express = require('express');
 const app = express();
 
+app.set("port", process.env.PORT || 3000)
+
+app.listen(app.get("port"), () => console.log("Server Start in http://localhost"+app.get("port")));
+
 app.use(express.static(path.resolve(__dirname, "./public")));
-app.listen(3000, ()=> console.log("Server Start in http://localhost:3000"));
 
 app.get("/", (req, res)=> res.sendFile(path.resolve(__dirname, "./views", "home.html")));
