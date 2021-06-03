75a7da186ea681be71e386195d9fa95662e10d9e
Federico Wagner <federico.wagner@hotmail.com>
2021-06-03 17:39:54 -0300

    Entrega Final

--- app.js
+++ app.js
@@ -10,3 +10,7 @@ app.listen(app.get("port"), () => console.log("Server Start in http://localhost"
 app.use(express.static(path.resolve(__dirname, "./public")));
 
 app.get("/", (req, res)=> res.sendFile(path.resolve(__dirname, "./views", "home.html")));
+
+app.get("/login", (req, res) => res.sendFile(path.resolve(__dirname, "./views/login.html")));
+
+app.get("/register", (req, res) => res.sendFile(path.resolve(__dirname, "./views/register.html")));
